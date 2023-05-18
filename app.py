144567f249a0c8904d1d794b368a2a74cca59b77
Mehrzad Jafari <59453002+mehrzadjafari@users.noreply.github.com>
2023-05-18 11:24:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,43 @@ import base64
 import pandas as pd
 import streamlit as st
 
-# Create a button to download the DataFrame
+def authenticate(username, password):
+    # Add your authentication logic here
+    # Return True if authentication is successful, False otherwise
+    return (username == "admin" and password == "password")
+
 def download_dataframe(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV file</a>'
     return href
 
+def main():
+    # Authentication
+    st.title("Authentication")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Authenticate"):
+        if authenticate(username, password):
+            st.success("Authentication successful!")
+            run_app()
+        else:
+            st.error("Authentication failed!")
 
+def run_app():
+    st.write("""
+    # govgr web scraping app
+    """)
 
+    # read df from github
+    github_data_path = "https://raw.githubusercontent.com/mehrzadjafari/solvency_greece/main/"
+    df = pd.read_csv(github_data_path + 'table_data.csv')
 
+    # show df in app
+    st.dataframe(df)
 
-st.write("""
-# govgr web scraping app
-""")
+    if st.button('Download'):
+        st.markdown(download_dataframe(df), unsafe_allow_html=True)
 
-# read df from github
-github_data_path = "https://raw.githubusercontent.com/mehrzadjafari/solvency_greece/main/"
-df = pd.read_csv(github_data_path + 'table_data.csv')
-
-# show df in app
-st.dataframe(df)
-
-if st.button('Download'):
-    st.markdown(download_dataframe(df), unsafe_allow_html=True)
+if __name__ == "__main__":
+    main()
