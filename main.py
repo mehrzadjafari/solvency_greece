aa4c161f183e4397615fc1639ed26fbaa7caeb9c
Mehrzad Jafari <59453002+mehrzadjafari@users.noreply.github.com>
2023-05-18 13:54:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import pandas as pd
 from datetime import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from extractor import extractor
+from extractor2 import extractor
+import asyncio
 
-# Configure Chrome options for headless browsing
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-
-# Create an instance of the ChromeDriver with headless options
-driver = webdriver.Chrome(options=chrome_options)
-
-# Call the extractor function with the provided driver and user date
-user_date = input('Enter the date:\nExample: 01/05/2023\n\n->  ')
+# Call the extractor function with the provided user date
+user_date = '12/05/2023'
 user_date = datetime.strptime(user_date, '%d/%m/%Y').date()
-table_data, header_values = extractor(driver, user_date)
 
-# Close the browser
-driver.quit()
+# Run the extractor function and get the table data
+table_data, header_values = asyncio.get_event_loop().run_until_complete(extractor(user_date))
 
 # Convert the table_data list to a DataFrame
 data = pd.DataFrame(table_data, columns=header_values)
-# print(data)
 
 # Save the data to a CSV file
-data.to_csv("table_data.csv", index=False)
+data.to_csv("table_data2.csv", index=False)
