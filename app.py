08182da08d8cd829ebb87e4e1f9fc6f771e4ad7f
Mehrzad Jafari <59453002+mehrzadjafari@users.noreply.github.com>
2023-05-18 12:56:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,6 @@ import streamlit as st
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from extractor import extractor
-
-# Configure Chrome options for headless browsing
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-
-# Create an instance of the ChromeDriver with headless options
-driver = webdriver.Chrome(options=chrome_options)
-
-# Call the extractor function with the provided driver and user date
-user_date = '12/05/2023'
-user_date = datetime.strptime(user_date, '%d/%m/%Y').date()
-table_data, header_values = extractor(driver, user_date)
-
-# Close the browser
-driver.quit()
-
-# Convert the table_data list to a DataFrame
-df = pd.DataFrame(table_data, columns=header_values)
 
 def authenticate(username, password):
     # Return True if authentication is successful, False otherwise
@@ -44,14 +23,35 @@ def main():
     if st.button("Authenticate"):
         if authenticate(username, password):
             st.success("Authentication successful!")
-            run_app(df)  # Pass the df DataFrame to run_app
+            run_app()
         else:
             st.error("Authentication failed!")
 
-def run_app(df):
-    st.write("""
-    # ΕΓΔΙΧ - scraping app
-    """)
+def run_app():
+    # Configure Chrome options for headless browsing
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+
+    # Specify the path to the ChromeDriver executable
+    driver_path = "https://raw.githubusercontent.com/mehrzadjafari/solvency_greece/main/chromedriver.exe"
+
+    # Create an instance of the ChromeDriver with headless options
+    driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+
+    # Call the extractor function with the provided driver and user date
+    user_date = '12/05/2023'
+    user_date = datetime.strptime(user_date, '%d/%m/%Y').date()
+    table_data, header_values = extractor(driver, user_date)
+
+    # Close the browser
+    driver.quit()
+
+    # Convert the table_data list to a DataFrame
+    df = pd.DataFrame(table_data, columns=header_values)
+
+    st.title("ΕΓΔΙΧ - scraping app")
 
     # show df in app
     st.dataframe(df)
