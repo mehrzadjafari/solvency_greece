5c33eb7d757d871bb9ea2e788d001921fec60999
Mehrzad Jafari <59453002+mehrzadjafari@users.noreply.github.com>
2023-06-03 15:32:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
+import base64
 import pandas as pd
 import streamlit as st
 from datetime import datetime, date
-import jwt
+import streamlit as st
+import io
+
 from extractor import extractor, download_dataframe
 
-def authenticate(username, password):
-    # Return True if authentication is successful, False otherwise
-    return (username == st.secrets["USERNAME"] and password == st.secrets["PASSWORD"])
+def main():
+    # Create two columns
+    col1, col2, col3 = st.columns([1, 2, 1])
 
-@st.cache
-def cached_extractor(user_date):
-    # Call the extractor function to get the table data and header values
-    return extractor(user_date)
-
-def login():
-    st.title("Login Page")
-    username = st.text_input("Username", key="username_input")
-    password = st.text_input("Password", type="password", key="password_input")
-
-    if st.button("Authenticate"):
-        if authenticate(username, password):
-            token = jwt.encode({"username": username}, st.secrets["SECRET_KEY"], algorithm="HS256")
-            st.experimental_set_query_params(token=token)
-            st.experimental_rerun()
-        else:
-            st.error("Authentication failed!")
-
-def content_page():
-    st.title("MMEA Transofrmation")
-    selected_date = st.date_input("Select a date", date.today())
+    # Center-align the title
+    col2.markdown("<h1 style='text-align: center;'>MMEA Transformation</h1>", unsafe_allow_html=True)
+    selected_date = st.date_input("From this date", date.today())
     user_date = datetime.combine(selected_date, datetime.min.time()).date()
+#     username = st.text_input("Username", key="username_input")
+#     password = st.text_input("Password", type="password", key="password_input")
+#     if st.button("Authenticate"):
+#         if authenticate(username, password):
+#             st.success("Authentication successful!")
+    run_app(user_date)
+#         else:
+#             st.error("Authentication failed!")
     
+
+
+def run_app(user_date):
+
     # Call the cached_extractor function to get the table data and header values
-    table_data, header_values = cached_extractor(user_date)
+    table_data, header_values = extractor(user_date)
 
     # Convert the table_data list to a DataFrame
     df = pd.DataFrame(table_data, columns=header_values)
@@ -45,21 +42,6 @@ def content_page():
         download_link = download_dataframe(df)
         st.markdown(download_link, unsafe_allow_html=True)
 
-def main():
-    # Get the current page from the query parameters
-    query_params = st.experimental_get_query_params()
-    token = query_params.get("token", [""])[0]
-    decoded_token = None
-
-    try:
-        decoded_token = jwt.decode(token, st.secrets["SECRET_KEY"], algorithms=["HS256"])
-    except jwt.exceptions.DecodeError:
-        pass
-
-    if decoded_token:
-        content_page()
-    else:
-        login()
 
 if __name__ == "__main__":
     main()
